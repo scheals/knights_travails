53f34d14b81975958ed9f706563776fa8ea73dc5
scheals <piecoo@gmail.com>
2022-03-28 20:28:24 +0200

    Add Knight movement rule

--- lib/knight.rb
+++ lib/knight.rb
@@ -2,5 +2,22 @@
 
 # This class handles a knight Chess piece.
 class Knight
+  def move(start, target)
+    return 'Invalid move!' unless valid_move?(start, target)
 
+    "Moved from #{start} to #{target}."
+  end
+
+  def valid_move?(start, target)
+    return true if [[1, 2], [2, 1]].include?(calculate_distances(start, target))
+
+    false
+  end
+
+  def calculate_distances(start, target)
+    distances = []
+    distances << (start[0] - target[0]).abs
+    distances << (start[1] - target[1]).abs
+    distances
+  end
 end
--- main.rb
+++ main.rb
@@ -5,5 +5,14 @@ require_relative 'lib/knight'
 
 knight = Knight.new
 board = Chessboard.new
+p knight.move([0, 0], [1, 2])
+p knight.move([0, 0], [-1, -2])
+p knight.move([0, 0], [1, -2])
+p knight.move([0, 0], [-1, 2])
+p knight.move([0, 0], [2, 1])
+p knight.move([0, 0], [-2, -1])
+p knight.move([0, 0], [2, -1])
+p knight.move([0, 0], [-2, 1])
+p knight.move([0, 0], [1, 4])
 board.put(knight)
 knight.moves([0, 0], [1, 2])
