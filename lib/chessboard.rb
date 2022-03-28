14cab3bb5e48bcbdbcff8e619afbec17118316a5
scheals <piecoo@gmail.com>
2022-03-28 19:58:21 +0200

    Fix Board class name

--- lib/chessboard.rb
+++ lib/chessboard.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 # This class handles a chessboard.
-class Board
+class Chessboard
   def initialize
-    @board = generate_chess_board
+    @board = generate_board
   end
 
-  def generate_chess_board
+  def generate_board
     board = []
     (0...8).to_a.repeated_permutation(2) { |permutation| board << permutation }
     board
--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require_relative 'lib/board'
+require_relative 'lib/chessboard'
 require_relative 'lib/knight'
 
 knight = Knight.new
-board = Board.new
+board = Chessboard.new
 board.put(knight)
 knight.moves([0, 0], [1, 2])
