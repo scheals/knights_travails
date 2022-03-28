bc684dfd8a962b95b47b897fc60155e9d0357670
scheals <piecoo@gmail.com>
2022-03-29 00:12:14 +0200

    Introduce moving Knight on board

--- lib/chessboard.rb
+++ lib/chessboard.rb
@@ -2,6 +2,8 @@
 
 # This class handles a chessboard.
 class Chessboard
+  attr_reader :board, :knight
+
   def initialize
     @board = generate_board
   end
@@ -10,5 +12,22 @@ class Chessboard
     board = []
     (0...8).to_a.repeated_permutation(2) { |permutation| board << permutation }
     board
+  end
+
+  def put(knight)
+    @knight = knight
+  end
+
+  def move(piece, target)
+    attempted_move = piece.move(target)
+    return 'Invalid move!' if attempted_move.nil? || out_of_bounds?(attempted_move)
+
+    target
+  end
+
+  def out_of_bounds?(coordinates)
+    return false if board.include?(coordinates)
+
+    true
   end
 end
--- main.rb
+++ main.rb
@@ -3,16 +3,12 @@
 require_relative 'lib/chessboard'
 require_relative 'lib/knight'
 
-knight = Knight.new
+knight = Knight.new([0, 0])
 board = Chessboard.new
-p knight.move([0, 0], [1, 2])
-p knight.move([0, 0], [-1, -2])
-p knight.move([0, 0], [1, -2])
-p knight.move([0, 0], [-1, 2])
-p knight.move([0, 0], [2, 1])
-p knight.move([0, 0], [-2, -1])
-p knight.move([0, 0], [2, -1])
-p knight.move([0, 0], [-2, 1])
-p knight.move([0, 0], [1, 4])
+p knight.move([1, 2])
+p knight.move([0, 2])
+p knight.possible_moves
 board.put(knight)
-knight.moves([0, 0], [1, 2])
+p board.move(knight, [2, 4])
+p board.move(knight, [1, 3])
+knight.moves([0, 0],[1, 2])
